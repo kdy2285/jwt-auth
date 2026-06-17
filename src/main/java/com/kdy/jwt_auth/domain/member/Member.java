1f6b7c93f62f86eaba643fc4d2046b29edc4bd57
DESKTOP-BITR5DO\DY <kdy542710@gmail.com>
2026-06-17 20:57:20 +0900

    feat: add member entity and repository

---
+++ src/main/java/com/kdy/jwt_auth/domain/member/Member.java
@@ -0,0 +1,37 @@
+package com.kdy.jwt_auth.domain.member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Role role;
+
+    public Member(String email, String password, Role role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/kdy/jwt_auth/domain/member/Role.java
@@ -0,0 +1,6 @@
+package com.kdy.jwt_auth.domain.member;
+
+public enum Role {
+    USER,
+    ADMIN
+}
